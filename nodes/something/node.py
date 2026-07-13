21cb72975fc41db9957295c2e93b394bf264c72e
Edward van Eechoud <41021650+Edwardvaneechoud@users.noreply.github.com>
2026-07-13 17:31:59 +0200

    Add something v1.0.0

---
+++ nodes/something/node.py
@@ -0,0 +1,54 @@
+import polars as pl
+from flowfile import node_designer as nd
+
+
+class SomethingSettings(nd.NodeSettings):
+    settings: nd.Section = nd.Section(
+        title="Give you rsettings",
+        columns=nd.ColumnSelector(
+            label="Pick your columns",
+            required=True,
+            multiple=True,
+            data_types=[
+                "String",
+            ],
+        ),
+        secret_selector_2=nd.SecretSelector(
+            label="secret_selector_2",
+        ),
+    )
+
+
+class Something(nd.CustomNodeBase):
+    node_name: str = "SOMETHING"
+    node_category: str = "Transform"
+    node_icon: str = "catalog_reader.svg"
+    title: str = "nospaces"
+    intro: str = "I want no spacess"
+    author: str = "edwardvaneechoud"
+    version: str = "1.0.0"
+    tags: list[str] = [
+        "text",
+    ]
+    example_inputs: list[dict[str, list]] = [
+        {
+            "column_1": [
+                "ed ward",
+            ],
+        },
+    ]
+    example_settings: dict[str, dict] = {
+        "settings": {
+            "columns": [
+                "column_1",
+            ],
+            "secret_selector_2": None,
+        },
+    }
+    settings_schema: SomethingSettings = SomethingSettings()
+
+    def process(self, *inputs: pl.LazyFrame) -> pl.LazyFrame:
+        # Get the first input LazyFrame
+        columns = self.settings_schema.settings.columns.value
+        lf = inputs[0]
+        return lf.with_columns([pl.col(c).str.replace(" ", "") for c in columns])
